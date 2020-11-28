b6acc42b580575769cf5146fdf2fc7433c8b7799
Solène Livran <s.livran@gmail.com>
2020-11-28 01:31:58 +0100

    feat: words choice page v1

--- template/words_choice.tpl.php
+++ template/words_choice.tpl.php
@@ -10,15 +10,18 @@
 <body>
 
 <div>Blabla choisissez les mots</div>
-<p>
-    <?php
-        foreach ($word_types as $word_type) {
-            echo "Choisissez un : ${word_type}<br>";
-    }
-    ?>
-</p>
+<form action="mad_libs.php" method="post" class="form">
+    <?php foreach ($word_types as $word_type) : ?>
+    <div class="form-group">
+        <label for="<?= $word_type ?>">Choisir un <?= $word_type ?> :</label>
+        <input type="text" name="<?= $word_type ?>" id="<?= $word_type ?>" required>
+    </div>
+    <?php endforeach ?>
+    <div class="form-group">
+        <input type="submit" value="Valider">
+    </div>
 
-<div>Blabla valider <a href="mad_libs.php">Valider</a></div>
+</form>
 
 </body>
 </html>
