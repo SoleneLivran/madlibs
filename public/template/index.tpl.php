0355e74ec3912a9a2f4d88e0318f98db4ce7c799
Solène Livran <s.livran@gmail.com>
2020-11-30 21:03:51 +0100

    feat: pages content

--- public/template/index.tpl.php
+++ public/template/index.tpl.php
@@ -5,23 +5,61 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <title>Histoires à trous - Accueil</title>
 </head>
 <body>
 
-<div>
-    <h1>Bienvenue sur le site</h1>
-    <p>Vous allez choisir des mots, au hasard, qui viendront remplir un texte pour un résultat...sans doute surprenant !</p>
-    <form action="words_choice.php" method="post">
-        <label for="story">Choix du texte qui sera complété par vos mots :</label>
-        <select name="story" id="story">
-            <!--Make all avalaible stories a dropdown option-->
-            <?php foreach ($available_stories as $story) : ?>
-                <option value="<?= $story['text'] ?>"><?= $story['title'] ?></option>
-            <?php endforeach ?>
-        </select>
-        <input type="submit" value="Valider">
-    </form>
+<div class="container">
+
+    <div class="page-title">
+        <h1>Bienvenue !</h1>
+    </div>
+
+    <div class="custom-shape-divider-top-1606758103">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+        </svg>
+    </div>
+
+    <div class="page-content">
+        <div class="rules rules-intro">
+            <p>
+                Voici un petit jeu pour créer des histoires.
+            <br>
+                Comment jouer ? C’est très simple !
+            </p>
+        </div>
+
+        <div class="rules rules-detail">
+            <p>
+                <i class="fas fa-arrow-right"></i> Choisis un thème, dans la liste en dessous (clique sur la petite flèche v pour voir tous les thèmes)
+            </p>
+            <p>
+                <i class="fas fa-arrow-right"></i> Ensuite, on va te demander plusieurs mots. Écris les premiers mots qui te passent par la tête !
+            </p>
+            <p>
+                <i class="fas fa-arrow-right"></i> Valide tes mots et tu verras une petite histoire, avec tes mots dedans !
+            </p>
+        </div>
+
+            <form action="words_choice.php" method="post">
+                <select name="story" id="story" required>
+                    <option value="" disabled selected>Choisis un thème ici</option>
+                    <!--Make all avalaible stories a dropdown option-->
+                    <?php foreach ($available_stories as $story) : ?>
+                        <option value="<?= $story['text'] ?>"><?= $story['title'] ?></option>
+                    <?php endforeach ?>
+                </select>
+                <input type="submit" value="Valider !">
+            </form>
+    </div>
+
 </div>
 
 </body>
--- public/template/words_choice.tpl.php
+++ public/template/words_choice.tpl.php
@@ -5,26 +5,61 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Histoires à trous - Choix des mots</title>
 </head>
 <body>
 
-<div>Blabla choisissez les mots</div>
-<form action="../mad_libs.php" " method="post" class="form">
-    <!--Create a form input for each needed word type in the text, for the user to choose the words-->
-    <?php foreach ($word_types as $word_type) : ?>
-    <div class="form-group">
-        <!--Remove "_" in word types names so it's more understandable for human users-->
-        <label for="<?= $word_type ?>">Choisir un <?= str_replace('_', ' ', $word_type) ?> :</label>
-        <input type="text" name="<?= $word_type ?>" id="<?= $word_type ?>" required>
+<div class="container">
+
+    <div class="page-title">
+        <h1>Choisis tes mots !</h1>
     </div>
-    <?php endforeach ?>
-    <!--hidden input to pass chosen text to next page. TODO : with session-->
-    <input type="hidden" name="text" id="text" value="<?= $text ?>">
-    <div class="form-group">
-        <input type="submit" value="Valider">
+
+    <div class="custom-shape-divider-top-1606758103">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+        </svg>
     </div>
-</form>
+
+    <div class="page-content">
+
+        <div class="rules">
+            <p>
+                Choisis les mots que tu veux. Fais attention à écrire le bon type de mots. 
+                <br>
+                Ce sont les mots que tu retrouveras dans ton histoire.  
+            </p>
+            <p>
+                Amuse-toi, et écris les premières idées qui te passent par la tête !
+            </p>
+        </div>
+
+        <div class="words-choice">
+            <p> Choisis...</p>
+            <form action="../mad_libs.php" " method="post" class="form">
+                <!--Create a form input for each needed word type in the text, for the user to choose the words-->
+                <?php foreach ($word_types as $word_type) : ?>
+                <div class="form-group">
+                    <!--Remove "_" in word types names so it's more understandable for human users-->
+                    <label for="<?= $word_type ?>">Choisir un <?= str_replace('_', ' ', $word_type) ?> :</label>
+                    <input type="text" name="<?= $word_type ?>" id="<?= $word_type ?>" required>
+                </div>
+                <?php endforeach ?>
+                <!--hidden input to pass chosen text to next page. TODO : with session-->
+                <input type="hidden" name="text" id="text" value="<?= $text ?>">
+                <div class="form-group">
+                    <input type="submit" value="Valider">
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+</div>
 
 </body>
 </html>
